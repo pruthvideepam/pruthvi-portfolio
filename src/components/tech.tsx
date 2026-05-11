1c07795dda0b95ae768dcc8a2b15aedc3a1b25ae
pruthvideepam <157585780+pruthvideepam@users.noreply.github.com>
2026-05-11 16:55:14 +0530

    Use static icons on mobile instead of 3D balls

--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -1,16 +1,26 @@
 import { BallCanvas } from "./canvas";
 import { TECHNOLOGIES } from "../constants";
 import { SectionWrapper } from "../hoc";
+import { useIsMobile } from "../hooks/useIsMobile";
 
 // Technologies
 export const Tech = () => {
+  const isMobile = useIsMobile();
+
   return (
     <SectionWrapper>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {/* Iterate over each technology */}
         {TECHNOLOGIES.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+          <div className="w-28 h-28 flex items-center justify-center" key={technology.name}>
+            {isMobile ? (
+              <img
+                src={technology.icon}
+                alt={technology.name}
+                className="w-16 h-16 object-contain"
+              />
+            ) : (
+              <BallCanvas icon={technology.icon} />
+            )}
           </div>
         ))}
       </div>
